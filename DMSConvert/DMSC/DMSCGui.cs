d7605d091c8b5bfee3ca2496e5e0e9ce96bc1aee
ChaoticWeg <shawn.lutch94@gmail.com>
2014-09-03 20:37:25 -0500

    Rewrite!
    Second commit of the rewrite branch (after the .gitignore)

---
+++ DMSConvert/DMSC/DMSCGui.cs
@@ -0,0 +1,122 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using UnityEngine;
+using KSP;
+
+namespace ChaoticWeg
+{
+    [KSPAddonImproved(KSPAddonImproved.Startup.RealTime)]
+    class DMSCGui : MonoBehaviour
+    {
+        private bool show = true;
+        private Rect windowPos = new Rect(50, 50, 50, 50);
+
+        private String txtDecimal = "0", txtDegrees = "0", txtMinutes = "0", txtSeconds = "0";
+        private float dec = 0f, deg = 0f, min = 0f, sec = 0f;
+        private bool btnDecToDms = false, btnDmsToDec = false;
+
+        public void Awake()
+        {
+            DMSConvert.Log("Awake, updating text fields");
+            UpdateTextFields(false);
+        }
+
+        public void OnGUI()
+        {
+            GUI.skin = HighLogic.Skin;
+            if (show)
+                windowPos = GUILayout.Window("DMSConvert".GetHashCode(), windowPos, debugWindow, "DMS/Decimal Conversion", GUILayout.ExpandWidth(true), GUILayout.ExpandHeight(true));
+
+            if (btnDmsToDec)
+            {
+                ParseAllFloats();
+                DMSConvert.DmsToDec(out dec, deg, min, sec);
+                UpdateTextFields(true);
+            }
+
+            if (btnDecToDms)
+            {
+                ParseAllFloats();
+                DMSConvert.DecToDms(dec, out deg, out min, out sec);
+                UpdateTextFields(true);
+            }
+        }
+
+        void debugWindow(int windowID)
+        {
+            GUILayout.BeginVertical();
+            GUILayout.FlexibleSpace();
+            GUILayout.BeginHorizontal();
+
+            // decimal
+            GUILayout.BeginVertical();
+            GUILayout.Label("Decimal", GUILayout.Width(75f));
+            txtDecimal = GUILayout.TextField(txtDecimal, GUILayout.Width(75f));
+            GUILayout.EndVertical();
+
+            GUILayout.Space(10f);
+
+            // button pane
+            GUILayout.BeginVertical();
+            btnDecToDms = GUILayout.Button("-->", GUILayout.Width(50f));
+            GUILayout.FlexibleSpace();
+            btnDmsToDec = GUILayout.Button("<--", GUILayout.Width(50f));
+            GUILayout.EndVertical();
+
+            GUILayout.Space(10f);
+
+            // dms side
+            GUILayout.BeginVertical();
+            GUILayout.Label("D", GUILayout.Width(50f));
+            txtDegrees = GUILayout.TextField(txtDegrees, GUILayout.Width(50f));
+            GUILayout.EndVertical();
+            GUILayout.BeginVertical();
+            GUILayout.Label("M", GUILayout.Width(50f));
+            txtMinutes = GUILayout.TextField(txtMinutes, GUILayout.Width(50f));
+            GUILayout.EndVertical();
+            GUILayout.BeginVertical();
+            GUILayout.Label("S", GUILayout.Width(50f));
+            txtSeconds = GUILayout.TextField(txtSeconds, GUILayout.Width(50f));
+            GUILayout.EndVertical();
+
+            GUILayout.EndHorizontal();
+            GUILayout.FlexibleSpace();
+            GUILayout.EndVertical();
+
+            GUI.DragWindow();
+        }
+
+        public bool AreFieldsStale()
+        {
+            return !txtDecimal.Equals(dec.ToString())
+                || !txtDegrees.Equals(deg.ToString())
+                || !txtMinutes.Equals(min.ToString())
+                || !txtSeconds.Equals(sec.ToString());
+        }
+
+        public void UpdateTextFields(bool log)
+        {
+            if (!AreFieldsStale())
+                return;
+
+            if (log)
+                DMSConvert.Log("Updating text fields");
+
+            txtDecimal = dec.ToString();
+            txtDegrees = deg.ToString();
+            txtMinutes = min.ToString();
+            txtSeconds = sec.ToString();
+        }
+
+        public void ParseAllFloats()
+        {
+            float.TryParse(txtDecimal, out dec);
+            float.TryParse(txtDegrees, out deg);
+            float.TryParse(txtMinutes, out min);
+            float.TryParse(txtSeconds, out sec);
+        }
+    }
+}
---
+++ DMSConvert/DMSC/DMSConvert.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using UnityEngine;
+
+namespace ChaoticWeg
+{
+    public class DMSConvert
+    {
+        public static void DecToDms(float dec, out float deg, out float min, out float sec)
+        {
+            deg = (float) Math.Floor(dec);
+            min = (float) Math.Floor((dec - deg) * 60);
+            sec = (float) Math.Round((((dec - deg) * 60) - min) * 60, 1);
+        }
+
+        public static void DmsToDec(out float dec, float deg, float min, float sec)
+        {
+            dec = RoundFloat(deg + (min / 60.0f) + (sec / 3600.0f), 4);
+        }
+
+        public static float RoundFloat(float f, int digits)
+        {
+            return (float) Math.Round(f, digits);
+        }
+
+        public static void Log(String message, LogType type = LogType.Log)
+        {
+            String msg = String.Format("[DMSConvert] {0}: {1}", Time.time.ToString("0.000"), message);
+            switch (type)
+            {
+                case LogType.Exception:
+                case LogType.Error:
+                    Debug.LogError(msg);
+                    break;
+
+                case LogType.Warning:
+                case LogType.Assert:
+                    Debug.LogWarning(msg);
+                    break;
+
+                case LogType.Log:
+                default:
+                    Debug.Log(msg);
+                    break;
+            }
+        }
+    }
+}
